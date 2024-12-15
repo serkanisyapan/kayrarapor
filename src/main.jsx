4111688ec7947221b34493650d47fee462c83e13
serkanisyapan <serkanisyapan1@hotmail.com>
2024-12-15 21:31:37 +0100

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import DetayTablo from './DetayTablo.jsx'
+import DetayTablo from './DetayTablo'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
